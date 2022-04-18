3a07a1a9b8ed60f13478871bf8b60fad82abf19a
zy_599 <767927053@qq.com>
2022-04-18 22:57:27 +0800

    修改配置

---
+++ src/helpers/util.ts
@@ -0,0 +1,11 @@
+import { values } from 'lodash'
+
+const toString = Object.prototype.toString
+
+export function isDate(val: any): val is Date {
+  return toString.call(val) === '[object Date]'
+}
+
+export function isObject(val: any): val is Object {
+  return val !== null && typeof val === 'object'
+}
